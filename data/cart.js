8fb10030dd3914ae12dbc961a4a29b4fc0cd051e
SuperSimpleDev <danjel.rexhaj@gmail.com>
2024-08-26 02:33:44 +0200

    Generate Html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productId:'36c64692-677f-4f58-b5ec-0dc2cf109e27',
+  quantity:2,
+}, {
+  productId:'aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f',
+  quantity:1,
+}];
 
 export function addToCart (productId, quantity, productName) {
   
