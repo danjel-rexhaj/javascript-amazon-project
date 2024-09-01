9fe9785385215a00df4a10bccded7de76df76d71
SuperSimpleDev <danjel.rexhaj@gmail.com>
2024-09-01 03:59:42 +0200

    Create automated tests for formatCurrency

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import {cart} from '../../data/cart.js';
+import {cart, calculateCartQuantity} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import {getDeliveryOption } from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
@@ -20,13 +20,19 @@ let shippingPriceCents = 0;
     const taxCents = totalBeforeTaxCents * 0.1;
     const totalCents = totalBeforeTaxCents + taxCents;
 
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+
     const paymentSummaryHTML = `
         <div class="payment-summary-title">
                 Order Summary
             </div>
 
             <div class="payment-summary-row">
-                <div>Items (3):</div>
+                <div>Items (${cartQuantity}):</div>
                 <div class="payment-summary-money">
                 $${formatCurrency(productPriceCents)}
                 </div>
@@ -66,4 +72,5 @@ let shippingPriceCents = 0;
         `;
 
         document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
 }
